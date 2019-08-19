fb12bfab49fd60762a8055a36b74fb19de1806a0
Ilya Vikulov <ilyavikulov99@gmail.com>
2019-08-19 12:36:30 +0300

    fixed 4.3 - homework

--- module4/3/HumanitarianAid/src/HumanitarianAid.java
+++ module4/3/HumanitarianAid/src/HumanitarianAid.java
@@ -4,42 +4,24 @@ public class HumanitarianAid {
     private static final int TRUCK_CAPACITY = 12;
     private static final int CONTAINER_CAPACITY = 27;
 
-    private static int boxCount;
-    private static int containerCount;
-    private static int truckCount;
-
-
     public static void main(String[] args) {
         System.out.print("Введите число ящиков: ");
         Scanner scanner = new Scanner(System.in);
-        boxCount = scanner.nextInt();
+        int boxCount = scanner.nextInt();
 
-        if (boxCount != 0 && boxCount % CONTAINER_CAPACITY == 0) {
-            containerCount = boxCount / CONTAINER_CAPACITY;
-        } else {
-            containerCount = boxCount / CONTAINER_CAPACITY + 1;
-        }
+        int containerCount = (int) Math.ceil( (double) boxCount / CONTAINER_CAPACITY);
+        int truckCount = (int) Math.ceil( (double) containerCount/TRUCK_CAPACITY);
 
-        if (containerCount % TRUCK_CAPACITY == 0) {
-            truckCount = containerCount/TRUCK_CAPACITY;
-        } else {
-            truckCount = containerCount/TRUCK_CAPACITY + 1;
-        }
-
-        int c = 1;                                              // Можно ли как-то избежать подобных переменных
-        int b = 1;                                              // счётчиков и сделать вывод более понятным в
-        for (int i = 1; i <= truckCount; ++i) {                 // плане кода?
-            System.out.println("\tГрузовик " + i);
-            int j = 0;
-            while (j < TRUCK_CAPACITY && c <= containerCount) {
-                System.out.println("\t\tКонтейнер " + c);
-                c++;
-                j++;
-                int k = 0;
-                while (k < CONTAINER_CAPACITY && b <= boxCount) {
-                    System.out.println("\t\t\t\t\tЯщик " + b);
-                    k++;
-                    b++;
+        int containerNumber = 1;
+        int boxNumber = 1;
+        for (int t = 1; t <= truckCount; ++t) {
+            System.out.println("\tГрузовик " + t);
+            for (int c = 0; c < TRUCK_CAPACITY && containerNumber <= containerCount; c++) {
+                System.out.println("\t\tКонтейнер " + containerNumber);
+                containerNumber++;
+                for (int b = 0; b < CONTAINER_CAPACITY && boxNumber <= boxCount; b++) {
+                    System.out.println("\t\t\t\t\tЯщик " + boxNumber);
+                    boxNumber++;
                 }
             }
         }
