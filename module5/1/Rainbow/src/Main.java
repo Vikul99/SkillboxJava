def4ed052ec5f5c9224d0f8cc0fc2b9127c737c6
Ilya Vikulov <ilyavikulov99@gmail.com>
2019-08-20 16:40:36 +0300

    fixed 5.1 homework

--- module5/1/Rainbow/src/Main.java
+++ module5/1/Rainbow/src/Main.java
@@ -3,8 +3,15 @@ public class Main {
         String text = "Каждый охотник желает знать, где сидит фазан";
 
         String[] colors = text.split(",?\\s+");
+
+        for (int i = 0; i < colors.length / 2; ++i) {
+            String tmp = colors[i];
+            colors[i] = colors[colors.length - i - 1];
+            colors[colors.length - i - 1] = tmp;
+        }
+
         for (int i = 0; i < colors.length; ++i) {
-            System.out.println(colors[colors.length - i - 1]);
+            System.out.println(colors[i]);
         }
     }
 }
--- module5/1/X/src/Main.java
+++ module5/1/X/src/Main.java
@@ -1,19 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String[][] mark = {
-                {"X     X"},
-                {" X   X"},
-                {"  X X"},
-                {"   X"},
-                {"  X X"},
-                {" X   X"},
-                {"X     X"},
-        };
+        System.out.print("Введите размер размер креста: ");
+        int markSize = new Scanner(System.in).nextInt();
+        System.out.println();
 
-        for (int i = 0; i < mark.length; ++i) {
-            for (int j = 0; j < mark[i].length; ++j) {
-                System.out.println("\t" + mark[i][j]);
+        for (int i = 0; i < markSize; ++i) {
+            for (int j = 0; j < markSize; ++j) {
+                if (j == i || j == markSize - i - 1) {
+                    System.out.print("X");
+                }
+                System.out.print(" ");
             }
+            System.out.println();
         }
     }
 }
