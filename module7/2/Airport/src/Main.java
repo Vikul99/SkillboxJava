1020b7197e93993f440eb6ef485a5d5e139bfcff
Ilya Vikulov <ilyavikulov99@gmail.com>
2019-09-04 21:28:35 +0300

    fixed 7.2 homework

--- module7/2/Airport/src/Main.java
+++ module7/2/Airport/src/Main.java
@@ -9,10 +9,11 @@ public class Main {
 
         Date date = new Date();
         date.setHours(date.getHours() + 2);
+        Date now = new Date();
 
         airport.getTerminals().stream()
                 .flatMap(t -> t.getFlights().stream())
-                .filter(flight -> flight.getDate().after(new Date()) && flight.getDate().before(date)
+                .filter(flight -> flight.getDate().after(now) && flight.getDate().before(date)
                         && flight.getType().equals(Flight.Type.DEPARTURE))
                 .sorted(Comparator.comparing(Flight::getDate))
                 .forEach(flight -> System.out.println(flight.getAircraft().getModel() + "\t" + flight));
