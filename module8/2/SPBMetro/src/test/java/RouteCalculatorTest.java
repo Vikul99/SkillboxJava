2b1d4f565a64925b9bea1068b5d01febd00883ab
Ilya Vikulov <ilyavikulov99@gmail.com>
2019-09-11 13:11:23 +0300

    fixed 8.2 - homework

--- module8/2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ module8/2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -2,39 +2,127 @@ import core.Line;
 import core.Station;
 import junit.framework.TestCase;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RouteCalculatorTest extends TestCase {
 
-    List<Station> route;
     RouteCalculator calculator;
+    StationIndex stationIndex;
+    Line lineOne;
+    Line lineTwo;
+    Line lineThree;
+    Station a;
+    Station z;
+    Station e;
+    Station d;
+    Station k;
+    Station b;
+    Station c;
+    Station f;
+    Station g;
+
+    /**
+     * Z - A ----- E          (line1: Z - A - E - D - K )
+     *     |       |          (line2: A - B - C - D)
+     *     B - C - D - K      (line3: C - F - G)
+     *         |
+     *         F - G
+     */
 
     @Override
     protected void setUp() throws Exception {
-        route = new ArrayList<>();
+        stationIndex = new StationIndex();
 
-        Line line1 = new Line(1, "Первая");
-        Line line2 = new Line(2, "Вторая");
+        lineOne = new Line(1, "Первая");
+        lineTwo = new Line(2, "Вторая");
+        lineThree = new Line(3, "Третья");
 
-        route.add(new Station("Петровская", line1));
-        route.add(new Station("Арбузная", line1));
-        route.add(new Station("Морковная", line2));
-        route.add(new Station("Яблочная", line2));
+        a = new Station("A", lineOne);
+        z = new Station("Z", lineOne);
+        e = new Station("E", lineOne);
+        d = new Station("D", lineOne);
+        k = new Station("K", lineOne);
+
+        b = new Station("B", lineTwo);
+        c = new Station("C", lineTwo);
+
+        f = new Station("F", lineThree);
+        g = new Station("G", lineThree);
+
+        lineOne.addStation(z);
+        lineOne.addStation(a);
+        lineOne.addStation(e);
+        lineOne.addStation(d);
+        lineOne.addStation(k);
+
+        lineTwo.addStation(a);
+        lineTwo.addStation(b);
+        lineTwo.addStation(c);
+        lineTwo.addStation(d);
+
+        lineThree.addStation(c);
+        lineThree.addStation(f);
+        lineThree.addStation(g);
+
+        stationIndex.addStation(a);
+        stationIndex.addStation(z);
+        stationIndex.addStation(e);
+        stationIndex.addStation(d);
+        stationIndex.addStation(k);
+        stationIndex.addStation(b);
+        stationIndex.addStation(c);
+        stationIndex.addStation(f);
+        stationIndex.addStation(g);
+
+        stationIndex.addLine(lineOne);
+        stationIndex.addLine(lineTwo);
+        stationIndex.addLine(lineThree);
+
+        List<Station> connectionStations = new ArrayList<>();
+        connectionStations.add(a);
+        connectionStations.add(b);
+        stationIndex.addConnection(connectionStations);
+        connectionStations.clear();
+
+        connectionStations.add(c);
+        connectionStations.add(d);
+        connectionStations.add(f);
+        stationIndex.addConnection(connectionStations);
+        connectionStations.clear();
+
+        calculator = new RouteCalculator(stationIndex);
     }
 
     public void testCalculateDuration() {
+        List<Station> route = calculator.getShortestRoute(e, c);
         double actual = RouteCalculator.calculateDuration(route);
-        double expected = 8.5;
+        double expected = 6;
         assertEquals(expected, actual);
     }
 
-    public void testGetShortestRoute() {
-        List<Station> actual = calculator.getShortestRoute();
-        actual.forEach(System.out::println);
+    public void testRouteOnTheLine() {
+        List<Station> route = calculator.getShortestRoute(z, e);
+        List<String> expected = Arrays.asList("Z", "A", "E");
+        assertEquals(expected, route.stream().map(s -> s.getName()).collect(Collectors.toList()));
     }
 
+    public void testRouteWithOneConnection() {
+        List<Station> route = calculator.getShortestRoute(e, c);
+        List<String> expected = Arrays.asList("E", "D", "C");
+        assertEquals(expected, route.stream().map(s -> s.getName()).collect(Collectors.toList()));
+    }
 
+    public void testRouteWithTwoConnections() {
+        List<Station> route = calculator.getShortestRoute(a, f);
+
+        route.forEach(s -> System.out.println(s.getName()));
+
+    }
+    
     @Override
     protected void tearDown() throws Exception {
 
