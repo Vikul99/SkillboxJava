aa74cead2864a856bb937d6ec4e26507d619b615
Ilya Vikulov <ilyavikulov99@gmail.com>
2019-09-13 10:54:43 +0300

    added logs

--- module8/2-3/SPBMetro/src/main/java/Main.java
+++ module8/2-3/SPBMetro/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import core.Line;
 import core.Station;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -12,6 +14,8 @@ import java.util.Scanner;
 
 public class Main
 {
+    private static Logger logger;
+
     private static String dataFile = "src/main/resources/map.json";
     private static Scanner scanner;
 
@@ -20,20 +24,26 @@ public class Main
     public static void main(String[] args)
     {
         RouteCalculator calculator = getRouteCalculator();
+        logger = LogManager.getRootLogger();
 
         System.out.println("Программа расчёта маршрутов метрополитена Санкт-Петербурга\n");
         scanner = new Scanner(System.in);
         for(;;)
         {
-            Station from = takeStation("Введите станцию отправления:");
-            Station to = takeStation("Введите станцию назначения:");
+            try {
+                Station from = takeStation("Введите станцию отправления:");
+                Station to = takeStation("Введите станцию назначения:");
+                logger.info("От " + from + " до " + to);
 
-            List<Station> route = calculator.getShortestRoute(from, to);
-            System.out.println("Маршрут:");
-            printRoute(route);
+                List<Station> route = calculator.getShortestRoute(from, to);
+                System.out.println("Маршрут:");
+                printRoute(route);
 
-            System.out.println("Длительность: " +
-                RouteCalculator.calculateDuration(route) + " минут");
+                System.out.println("Длительность: " +
+                        RouteCalculator.calculateDuration(route) + " минут");
+            } catch (Exception ex) {
+                logger.error(ex);
+            }
         }
     }
 
@@ -73,6 +83,7 @@ public class Main
             if(station != null) {
                 return station;
             }
+            logger.info("Станция не найдена :" + line);
             System.out.println("Станция не найдена :(");
         }
     }
