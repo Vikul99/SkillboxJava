ca76723ff649a7c62d9f06f2655f0658f195d2c4
Ilya Vikulov <ilyavikulov99@gmail.com>
2019-10-27 21:47:32 +0300

    fixed 9.5 - homework

--- module9/5/MSCMetro/src/Main.java
+++ module9/5/MSCMetro/src/Main.java
@@ -1,16 +1,75 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static final String LINK = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
+
+    public static final String LINK = "https://ru.wikipedia.org/wiki/Список_станций_Московского_метрополитена";
+    public static final String FILE_NAME = "data/MSCMetro.json";
+    public static final String LINE_NAME_REGEX = "\"[а-яА-я]+?.+?\\s[а-яА-я]+\\s?[а-яА-я]+\"";
+
+
     public static void main(String[] args) {
         try {
-            Element elements = Jsoup.connect(LINK).get().select("table").get(3);
-            System.out.println(elements);
+            Element table = Jsoup.connect(LINK).maxBodySize(0).get().select("table").get(3);
+            Elements rows = table.select("tr");
+
+            TreeMap<String, String> lineMap = new TreeMap<>();
+            for (int i = 1; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+                String lineNumber = cols.get(0).text().substring(0, cols.get(0).text().length() - 2);
+                Matcher m = Pattern.compile(LINE_NAME_REGEX).matcher(cols.get(0).html());
+                if (m.find()) {
+                    lineMap.put(lineNumber, m.group().substring(1, m.group().length() - 1));
+                }
+            }
+
+            JSONObject obj = new JSONObject();
+            obj.put("stations", getStations(lineMap, rows));
+            obj.put("lines", getLines(lineMap));
+            Files.write(Paths.get(FILE_NAME), obj.toJSONString().getBytes());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static JSONArray getLines(Map<String, String> lineMap) {
+        JSONArray lines = new JSONArray();
+        for (String lineNumber: lineMap.keySet()) {
+            JSONObject line = new JSONObject();
+            line.put("number", lineNumber);
+            line.put("name", lineMap.get(lineNumber));
+            lines.add(line);
+        }
+        return lines;
+    }
+
+    public static JSONObject getStations(Map<String, String> lineMap , Elements rows) {
+        JSONObject stations = new JSONObject();
+        for (String lineNumber: lineMap.keySet()) {
+            JSONArray list = new JSONArray();
+            for (int i = 1; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+                String currLine = cols.get(0).text().substring(0, cols.get(0).text().length() - 2);
+                if (lineNumber.equals(currLine)) {
+                    String station = cols.get(1).text();
+                    list.add(station);
+                }
+            }
+            stations.put(lineNumber, list);
+        }
+        return stations;
     }
 }
